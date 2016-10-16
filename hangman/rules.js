679c89de7401b5897baa252d1d6fca1405c958ba
Aida Zuniga <aida.zuniga.e@gmail.com>
2016-10-15 22:53:09 -0400

    hangman edit

--- hangman/rules.js
+++ hangman/rules.js
@@ -1,10 +1,35 @@
 //HANGMAN
 
-var words = ['around', 'astonishing', 'bikini', 'bookworm', 'cat', 'elephant', 'fitness', 
-			'galaxy', 'jigsaw', 'kiwi', 'microwave', 'music', 'nightclub',
-			'pixel', 'pneumonia', 'robot', 'rainbow', 'swing', 'scientist', 'subway', 
-			'tequila', 'tree', 'unknown', 
-			'vodka', 'waltz', 'whiskey', 'youthful', 'zizag', 'zombie']
+var words = ['aida', 'adversary', 'algorithmn', 'anatomy','around', 'artificial', 'astonishing',
+	'beagle', 'bikini', 'bookworm', 'brick',
+    'canal', 'cardio', 'cast', 'cat', 'controller', 'communication', 'computation', 'customs',
+    'doll', 'damange', 'dancing',
+    'eager', 'easygoing', 'elephant', 'encapsulation', 'essential',
+    'equine', 'exchange', 'exist', 'express',
+    'faculty', 'film', 'fireplace', 'fitness', 'focus',
+    'galaxy', 'gate', 'generally', 'genetics', 'genuine', 'graphics',
+    'handwriting', 'harvest', 'horses',
+    'ideal', 'illustrate', 'imagination', 'impact', 'import', 'induction', 'infrastructure',
+    'interface', 'inheritance',
+    'java', 'jelly', 'jewelry', 'jigsaw', 'journey',
+    'kickoff', 'kiwi', 'knowledge',
+    'language', 'latina', 'latino', 'laughter', 'logic',
+    'machine', 'merengue','microwave', 'module', 'music',
+    'nearby', 'networks', 'nightclub',
+    'object', 'offer', 'optimization', 'oriented',
+    'package', 'painter', 'paper', 'parallel', 'pixel', 'pneumonia', 'programming', 'prototype',
+    'python',
+    'quaity', 'question', 'quotation',
+    'ratio', 'rational', 'raw', 'robot', 'rainbow',
+    'sarcastic', 'scale', 'search', 'swing', 'scientist', 'schedule', 'scope', 'scoreboard',
+    'silicon', 'software', 'subway',
+    'target', 'teammate', 'technical', 'tequila', 'thrift', 'tree',
+    'undergraduate', 'unique', 'unknown', 'user',
+    'vacation', 'valley', 'valuable', 'variety', 'vegetable', 'verdict', 'version',
+    'veterinarian', 'view', 'vodka',
+    'waltz', 'wander', 'whiskey',
+    'youthful',
+    'zizag', 'zombie']
 
 //Choose random words
 function chooseWord(){
